774d82f2f575a8b6e1be41e54b4a62d089a01a7f
Isham <isham.rahman02@gmail.com>
2019-11-03 17:07:51 +0600

    trial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var oHihat = new Audio("sounds/Open-hihat.WAV");
+      oHihat.play();
+
+
+      break;
+    case "a":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "s":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "d":
+      var cHihat = new Audio("sounds/Closed-hihat.WAV");
+      cHihat.play();
+      break;
+    case "j":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "k":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+
+  }
+
+}
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+});
+
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+  });
+}
